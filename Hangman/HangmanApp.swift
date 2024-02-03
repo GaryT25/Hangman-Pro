f94be4b7df40f27b8a01f628176498e4bdb7b9d9
Gary Thompson <truegarythompson@gmail.com>
2024-02-02 21:30:14 -0500

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Gary Thompson on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
